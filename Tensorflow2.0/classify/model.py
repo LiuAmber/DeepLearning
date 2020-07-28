c6bf005a7dbc1498baa989215a9731fb0133f49f
LiuAmber <lwhAmber@outlook.com>
2020-07-28 10:21:39 +0800

    [update] myself vgg model

--- Tensorflow2.0/classify/model.py
+++ Tensorflow2.0/classify/model.py
@@ -2,16 +2,80 @@ import tensorflow as tf
 from tensorflow.keras import layers
 def myModel(input_shape,load_model = True):
     if load_model == True:
-        VGG16 = tf.keras.applications.VGG16(weights = 'imagenet',include_top=False)
-        VGG16.trainable =False
+        myVGG = tf.keras.applications.VGG16(weights = 'imagenet',include_top=False)
+        myVGG.trainable =False
     else:
-        pass
+        myVGG = tf.keras.Sequential()
+
+        myVGG.add(layers.Conv2D(64,(3,3),padding='same'))
+        myVGG.add(layers.BatchNormalization())
+        myVGG.add(layers.Activation('relu'))
+
+        myVGG.add(layers.Conv2D(64,(3,3),padding='same'))
+        myVGG.add(layers.BatchNormalization())
+        myVGG.add(layers.Activation('relu'))
+
+        myVGG.add(layers.MaxPooling2D())
+
+        myVGG.add(layers.Conv2D(128,(3,3),padding='same'))
+        myVGG.add(layers.BatchNormalization())
+        myVGG.add(layers.Activation('relu'))
+
+        myVGG.add(layers.Conv2D(128,(3,3),padding='same'))
+        myVGG.add(layers.BatchNormalization())
+        myVGG.add(layers.Activation('relu'))
+
+        myVGG.add(layers.MaxPooling2D())
+
+        myVGG.add(layers.Conv2D(256,(3,3),padding='same'))
+        myVGG.add(layers.BatchNormalization())
+        myVGG.add(layers.Activation('relu'))
+
+        myVGG.add(layers.Conv2D(256,(3,3),padding='same'))
+        myVGG.add(layers.BatchNormalization())
+        myVGG.add(layers.Activation('relu'))
+
+        myVGG.add(layers.Conv2D(256, (1, 1), padding='same'))
+        myVGG.add(layers.BatchNormalization())
+        myVGG.add(layers.Activation('relu'))
+
+        myVGG.add(layers.MaxPooling2D())
+
+        myVGG.add(layers.Conv2D(512, (3, 3), padding='same'))
+        myVGG.add(layers.BatchNormalization())
+        myVGG.add(layers.Activation('relu'))
+
+        myVGG.add(layers.Conv2D(512, (3, 3), padding='same'))
+        myVGG.add(layers.BatchNormalization())
+        myVGG.add(layers.Activation('relu'))
+
+        myVGG.add(layers.Conv2D(512, (1, 1), padding='same'))
+        myVGG.add(layers.BatchNormalization())
+        myVGG.add(layers.Activation('relu'))
+
+        myVGG.add(layers.MaxPooling2D())
+
+        myVGG.add(layers.Conv2D(512, (3, 3), padding='same'))
+        myVGG.add(layers.BatchNormalization())
+        myVGG.add(layers.Activation('relu'))
+
+        myVGG.add(layers.Conv2D(512, (3, 3), padding='same'))
+        myVGG.add(layers.BatchNormalization())
+        myVGG.add(layers.Activation('relu'))
+
+        myVGG.add(layers.Conv2D(512, (1, 1), padding='same'))
+        myVGG.add(layers.BatchNormalization())
+        myVGG.add(layers.Activation('relu'))
+
+
+
     inputs = tf.keras.layers.Input(shape=input_shape)
-    x = VGG16(inputs)
+    x = myVGG(inputs)
     x = layers.GlobalAveragePooling2D()(x)
-    x = layers.Dense(2048, activation='relu')(x)
-    x = layers.Dense(256, activation='relu')(x)
-    outputs = layers.Dense(10,activation='softmax')(x)
+    x = layers.Dense(4096, activation='relu')(x)
+    x = layers.Dense(4096, activation='relu')(x)
+    x = layers.Dense(1000, activation='relu')(x)
+    outputs = layers.Dense(20,activation='softmax')(x)
     model = tf.keras.models.Model(inputs=inputs,
                                   outputs=outputs)
     return model
